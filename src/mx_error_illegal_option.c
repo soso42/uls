cbab729934ab784c97464e0314d7f56f1480cb5f
Soso Janashvili <sosojanashvili@gmail.com>
2020-09-06 12:31:20 +0400

    Added error management f-s

---
+++ src/mx_error_illegal_option.c
@@ -0,0 +1,12 @@
+//
+// Created by Soso Janashvili on 06.09.2020.
+//
+
+#include "../inc/uls.h"
+
+void mx_error_illegal_option(const char *ch) {
+    mx_printerror("uls illegal option -- ");
+    mx_printerror(ch);
+    mx_printerror("\nusage: uls [-ACGRSTaclmrtu1] [file ...]\n");
+    exit(1);
+}
---
+++ src/mx_error_no_such_file.c
@@ -0,0 +1,11 @@
+//
+// Created by Soso Janashvili on 06.09.2020.
+//
+
+#include "../inc/uls.h"
+
+void mx_error_no_such_file(char *filename) {
+    mx_printstr("ls: ");
+    mx_printstr(filename);
+    mx_printstr(": No such file or directory\n");
+}
